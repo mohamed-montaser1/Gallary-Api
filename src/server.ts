58d22f16e58b737795405fc157d62896da5b1ba5
mohamed montaser <mastrocoding@gamil.com | muslim.mohamed.montaser@gmail.com>
2023-06-24 06:04:09 +0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import { Express } from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
 import * as loginRouter from "./routes/auth/login";
+import * as cors from "cors";
 config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 const db_url = process.env.DB_URL || "mongodb://0.0.0.0:27017/test";
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 app.use("/api/auth", loginRouter.default);
 
 app.listen(port, () =>
